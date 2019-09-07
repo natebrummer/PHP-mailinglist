75fbb91407e130bd1a215f7ee621c199ab12d046
Nate Brummer <natebrummer31@gmail.com>
2019-09-07 13:39:57 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+   
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500i&display=swap" rel="stylesheet">
+    <title>Brumatti</title>
+</head>
+<body>
+
+<h1>Mailing List</h1>
+
+  
+
+<div class="copyright">
+    <marquee behavior="scroll" direction="up">
+    twfs.studio &copy;</marquee>
+</div>
+
+<h1 class="out">
+    <span class="word">Sign Up</span>
+</h1>
+
+<button class="contact">
+    <a href="mailto:natebrummer31@gmail.com">Contact
+</a>
+</button>
+
+<button class="contact2">
+    <a href="https://brumatti.bandcamp.com/track/la-houston">Listen</a>
+</button>
+
+<div class="maillist">
+Join the mailing list: <input type="text" name="email" value="<?php $_GET [email];?>">
+</div>
+
+
+<footer>
+    2019 ___ Brumatti
+</footer>
+</body>
+
+<script src="index.js">
+</script>
+
+</html>
